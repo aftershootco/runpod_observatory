f487f0da60c0e0e010fc79b10d088c0d7d10cdb1
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2025-05-12 13:21:38 +0530

    ENH: Add machine telemetry

--- sample_script.py
+++ sample_script.py
@@ -5,7 +5,23 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
+
 op = os.path
+
+QUERY = """query Pod {
+  pod(input: {podId: "%s"}) {
+    id
+    name
+    latestTelemetry {
+      cpuUtilization
+      memoryUtilization
+      averageGpuMetrics {
+        percentUtilization
+        memoryUtilization
+      }
+    }
+  }
+}"""
 
 
 def auth():
@@ -16,15 +32,28 @@ def auth():
     else:
         creds = json.loads(key)
     creds = Credentials.from_service_account_info(
-        creds,
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        creds, scopes=["https://www.googleapis.com/auth/spreadsheets"]
     )
     return gspread.authorize(creds)
 
 
+def get_telemetry(pod_id):
+    query = QUERY % pod_id
+    result = runpod.api.graphql.run_graphql_query(query)
+    latest_telemetry = result['data']['pod']['latestTelemetry']
+    cpu_util = latest_telemetry['cpuUtilization']
+    mem_util = latest_telemetry['memoryUtilization']
+    gpu_metrics = latest_telemetry['averageGpuMetrics']
+    return [
+        cpu_util,
+        mem_util,
+        gpu_metrics['percentUtilization'],
+        gpu_metrics['memoryUtilization'],
+    ]
+
+
 def sample(client):
-    spreadsheet = client.open_by_key(
-        "1_X8YlG4UBTVJfAIuknew64BZECCcBWrjoA_FibLYmDQ")  # from URL
+    spreadsheet = client.open_by_key("1_X8YlG4UBTVJfAIuknew64BZECCcBWrjoA_FibLYmDQ")  # from URL
     worksheet = spreadsheet.worksheet("Sheet1")  # or by title
     runpod.api_key = os.environ['RUNPOD_KEY']
     pods = runpod.get_pods()
@@ -34,9 +63,8 @@ def sample(client):
         del pod['env']
         del pod['machine']
         pod['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        payload.append(list(pod.values()))
-    worksheet.append_rows(payload, value_input_option="USER_ENTERED",
-                          table_range="A1")
+        payload.append(list(pod.values()) + get_telemetry(pod['id']))
+    worksheet.append_rows(payload, value_input_option="USER_ENTERED", table_range="A1")
 
 
 if __name__ == "__main__":
